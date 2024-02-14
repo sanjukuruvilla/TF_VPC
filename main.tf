04e1bbae16ddd8fb9b7b244cc70da2da082b02a0
Sanju Kuruvilla <sanjukuruvila@gmail.com>
2024-02-14 15:39:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,88 +2,30 @@ resource "aws_vpc" "my_vpc" {
   cidr_block = var.vpc_cidr_block
 }
 
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
-}
-
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_igw.id
-  }
-}
-
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
-}
-
-resource "aws_subnet" "public_subnets" {
-  count             = length(var.public_subnet_cidr_blocks)
+resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = var.public_subnet_cidr_blocks[count.index]
-  map_public_ip_on_launch = true
-  availability_zone = var.aws_region
-  tags = {
-    Name = "public-subnet-${count.index + 1}"
-  }
+  cidr_block        = var.public_subnet_cidr_block
+  availability_zone = "us-east-1a"  # Adjust AZ as needed
 }
 
-resource "aws_route_table_association" "public_associations" {
-  count          = length(var.public_subnet_cidr_blocks)
-  subnet_id      = aws_subnet.public_subnets[count.index].id
-  route_table_id = aws_route_table.public_route_table.id
-}
-
-resource "aws_subnet" "private_subnets" {
-  count             = length(var.private_subnet_cidr_blocks)
+resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = var.private_subnet_cidr_blocks[count.index]
-  availability_zone = var.aws_region
-  tags = {
-    Name = "private-subnet-${count.index + 1}"
-  }
+  cidr_block        = var.private_subnet_cidr_block
+  availability_zone = "us-east-1b"  # Adjust AZ as needed
 }
 
-resource "aws_route_table_association" "private_associations" {
-  count          = length(var.private_subnet_cidr_blocks)
-  subnet_id      = aws_subnet.private_subnets[count.index].id
-  route_table_id = aws_route_table.private_route_table.id
-}
-
-resource "aws_security_group" "my_sg" {
-  vpc_id = var.vpc_id
-
-  dynamic "ingress" {
-    for_each = var.security_group_ingress_rules
-    content {
-      from_port   = ingress.value.from_port
-      to_port     = ingress.value.to_port
-      protocol    = ingress.value.protocol
-      cidr_blocks = ingress.value.cidr_blocks
-    }
-  }
-
-  dynamic "egress" {
-    for_each = var.security_group_egress_rules
-    content {
-      from_port   = egress.value.from_port
-      to_port     = egress.value.to_port
-      protocol    = egress.value.protocol
-      cidr_blocks = egress.value.cidr_blocks
-    }
-  }
-}
-
-resource "aws_instance" "my_instance" {
+resource "aws_instance" "public_instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  subnet_id     = aws_subnet.private_subnets[0].id
-  security_groups = [aws_security_group.my_sg.name]
+  subnet_id     = aws_subnet.public_subnet.id
   key_name      = var.key_pair_name
+  user_data     = var.user_data
 }
 
-output "instance_public_ip" {
-  value = aws_instance.my_instance.public_ip
+resource "aws_instance" "private_instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.private_subnet.id
+  key_name      = var.key_pair_name
+  user_data     = var.user_data
 }
