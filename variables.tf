f53889737d96e8e2e4bd201aa2cdb4012a946205
Sanju Kuruvilla <sanjukuruvila@gmail.com>
2024-02-14 14:30:45 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,43 +1,52 @@
 variable "aws_region" {
   description = "The AWS region where resources will be created"
   type        = string
-  default     = "your_preferred_region"
 }
 
 variable "vpc_cidr_block" {
-  description = "Class C CIDR block for VPC"
+  description = "CIDR block for VPC"
   type        = string
-  default     = "10.0.0.0/24"
+}
+
+variable "security_group_ingress_rules" {
+  description = "List of ingress rules for the security group"
+  type        = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default     = [{
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }]
 }
 
 variable "public_subnet_cidr_blocks" {
   description = "CIDR blocks for public subnets"
-  type        = list(string)
-  default     = ["10.0.0.0/26", "10.0.0.64/26"]
+  type        = list(string)  //using list(string for multiple subnets) or use string for single subnet
 }
 
 variable "private_subnet_cidr_blocks" {
   description = "CIDR blocks for private subnets"
-  type        = list(string)
-  default     = ["10.0.0.128/26", "10.0.0.192/26"]
+  type        = list(string)  //using list(string for multiple subnets) or use string for single subnet
 }
 
 variable "key_pair_name" {
   description = "Name of the SSH key pair for EC2 instance"
   type        = string
-  default     = "your_key_pair_name"
 }
 
 variable "ami_id" {
   description = "AMI ID for EC2 instance"
   type        = string
-  default     = "your_ami_id"
 }
 
 variable "instance_type" {
   description = "Instance type for EC2 instance"
   type        = string
-  default     = "your_instance_type"
 }
 
 variable "user_data" {
